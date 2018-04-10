8f594214c9f7bbdb08dc3330d2509317ee89791e
Adrien Vande Casteele <adrien@MacBook-Pro-de-Adrien.local>
2018-04-10 15:02:03 +0200

    hero-page

--- components/heros.php
+++ components/heros.php
@@ -7,7 +7,9 @@
     $hero_infos = getHeroInfos($id_list[$id]['hero']);
     $actor_infos = getActorInfos($id_list[$id]['actor']);
 
-    $doc_title = $hero_infos->name
+    $doc_title = $hero_infos->name;
+
+    include('header.php');
 ?>
 
 <!DOCTYPE html>
@@ -21,16 +23,34 @@
     <link rel="stylesheet" href="../style/style.css">
 </head>
 <body class="hero-page">
-    <img class="hero-image" src="../images/hero_actor/hero_<?= $id?>.png" alt="<?= $hero_infos->name?>">
-    <img class="hero-image-color" src="../images/hero_actor/hero_<?= $id?>.png" alt="<?= $hero_infos->name?>">
-
+    <nav class="hero-nav">
+    </nav>
+    <div class="hero-image">
+        <img class="normal-image" src="../images/hero_actor/hero_<?= $id?>.png" alt="<?= $hero_infos->name?>">
+        <img class="color-image-1" src="../images/hero_actor/hero_<?= $id?>.png" alt="<?= $hero_infos->name?>">
+        <img class="color-image-2" src="../images/hero_actor/hero_<?= $id?>.png" alt="<?= $hero_infos->name?>">
+    </div>
     <div class="content">
         <h1><?= $hero_infos->name?></h1>
         <h3>Played by <?= $actor_infos->name?></h3>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda ducimus incidunt dignissimos animi esse amet autem, quasi omnis placeat impedit voluptate possimus eum earum aliquam repellat illum, pariatur commodi dolorum!</p>
-        <p>Place of birth : <?= $hero_infos->birth?></p>
-        <p>First appearance : <?= $hero_infos->origin?></p>
+        <p class="main-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda ducimus incidunt dignissimos animi esse amet autem, quasi omnis placeat impedit voluptate possimus eum earum aliquam repellat illum, pariatur commodi dolorum!</p>
+        <p>PLACE OF BIRTH : <?= $hero_infos->birth?></p>
+        <p>FIRST APPEARANCE : <?= $hero_infos->origin?></p>
     </div>
+    <div class="buttons-container">
+            <div class="switch-hero active">
+                <div class="bar">
+                    <div class="fill-bar"></div>
+                </div>
+                <span class="switch-link">character</span>
+            </div>
+            <div class="switch-hero">
+                <div class="bar">
+                    <div class="fill-bar"></div>
+                </div>
+                <span class="switch-link">actor</span>
+            </div>
+        </div>
     <div class="red-background"></div>
 </body>
 </html>
